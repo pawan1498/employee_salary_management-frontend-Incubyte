ace6b424ae176073ca2492ac37ea3bad0cdc1565
Pawan Sharma <pawan.sharma@w3villa.com>
2026-09-18 15:28:10 +0530

    Add country and department filter dropdowns with an All option.

---
+++ src/components/FilterSelect.tsx
@@ -0,0 +1,31 @@
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+
+type FilterSelectProps = {
+  label: string;
+  value: string;
+  options: readonly string[];
+  onChange: (value: string) => void;
+  minWidth?: number;
+};
+
+export function FilterSelect({
+  label,
+  value,
+  options,
+  onChange,
+  minWidth = 200,
+}: FilterSelectProps) {
+  return (
+    <FormControl size="small" sx={{ minWidth }}>
+      <InputLabel>{label}</InputLabel>
+      <Select label={label} value={value} onChange={(event) => onChange(event.target.value)}>
+        <MenuItem value="">All</MenuItem>
+        {options.map((option) => (
+          <MenuItem key={option} value={option}>
+            {option}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+}
---
+++ src/constants/filters.ts
@@ -0,0 +1,15 @@
+export const COUNTRIES = [
+  "United States",
+  "United Kingdom",
+  "India",
+  "Germany",
+  "Canada",
+] as const;
+
+export const DEPARTMENTS = [
+  "Engineering",
+  "People",
+  "Finance",
+  "Sales",
+  "Operations",
+] as const;
